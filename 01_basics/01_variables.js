28a7a59eef9a76ec18037db07d6c047e503b47ef
Gwadatkar <107981227+Gwadatkar@users.noreply.github.com>
2025-05-03 12:18:34 +0000

    practice

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const AccountId=1212
+var accnName="Gaurav"
+let email="abc@gmail.com"
+let vodafone;
+
+//AccountId=212
+
+console.log(AccountId);
+console.table([AccountId,accnName,email,AccountId,vodafone])
---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,41 @@
+// Examples of Data Types in JavaScript
+
+// 1. String
+let name = "John Doe";
+console.log("String:", name);
+
+// 2. Number
+let age = 25;
+console.log("Number:", age);
+
+// 3. Boolean
+let isStudent = true;
+console.log("Boolean:", isStudent);
+
+// 4. Undefined
+let undefinedVariable;
+console.log("Undefined:", undefinedVariable);
+
+// 5. Null
+let emptyValue = null;
+console.log("Null:", emptyValue);
+
+// 6. Object
+let person = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 30,
+};
+console.log("Object:", person);
+
+// 7. Array (a type of object)
+let colors = ["red", "green", "blue"];
+console.log("Array:", colors);
+
+// 8. Symbol (unique and immutable value)
+let uniqueId = Symbol("id");
+console.log("Symbol:", uniqueId);
+
+// 9. BigInt (for large integers)
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt:", bigNumber);
